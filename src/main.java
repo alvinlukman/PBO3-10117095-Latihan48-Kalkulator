d6cf54e077a9f4c7392030cc3b5e3a65e8a017ff
alvinlukmanNH <galinx20gmail.com>
2018-10-22 23:23:55 +0700

    kalkulator

---
+++ src/main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  :Alvin Lukman Nulhakim
+ * NIM   :10117095    
+ * KELAS :IF-3
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kalkulator objKal = new Kalkulator();
+        objKal.setValue1(7.0);
+        objKal.setValue2(5.0);
+        System.out.println("VALUE 1 = "+objKal.getValue1());
+        System.out.println("VALUE 2 = "+objKal.getValue2());
+        objKal.setNameProject();
+        objKal.setNoteProject("");
+        System.out.println("Result Add is = "+objKal.add());
+        System.out.println("Result Minus is = "+objKal.minus());
+        System.out.println("Result Multiple is = "+objKal.multiplication());
+        System.out.println("Result Division is = "+objKal.division());
+        
+    }
+    
+}
